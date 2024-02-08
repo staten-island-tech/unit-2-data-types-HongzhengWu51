d889f08cb5160946e6f98d49c3bf86c4e7c462f1
HongzhengWu51 <158204281+HongzhengWu51@users.noreply.github.com>
2024-02-08 12:28:08 -0500

    changes

--- service.py
+++ service.py
@@ -1,10 +1,42 @@
 bill=(input("Bill:"))
 service=(input("How was the service?:"))
+
+x="0%"
+y="15%"
+z="20%"
+w="25%"
+
+
+tip=0*bill
+tip1=0.15*bill
+tip2=0.20*bill
+tip3=0.25*bill
+
+
+total=0*bill+bill
+total1=0.15*bill+bill
+total2=0.20*bill+bill
+total3=0.25*bill+bill
+
+
 if service=="Bad":
-    tip=0
+    print(f"Tip is {x}")
+    print(tip)
+    print(total)
 elif service=="Okay":
-    tip=15
+    print(f"Tip is {y}")
+    print(tip1)
+    print(total1)
 elif service=="Good":
-    tip=20
+    print(f"Tip is {z}")
+    print(tip2)
+    print(total2)
+
 elif service=="Great":
-    print('25%')
+    print(f"Tip is {w}")
+    print(tip3)
+    print(total3)
+   
+
+
+
---
+++ service1.py
@@ -0,0 +1,32 @@
+bill=(input("Bill:"))
+service=(input("How was the service?:"))
+
+x="0%"
+y="15%"
+z="20%"
+w="25%"
+
+
+
+
+
+
+if service=="Bad":
+    tip=0
+    print(f"Tip is {x}")
+    
+    
+elif service=="Okay":
+     print(f"Tip is {y}")
+     tip=0.15
+  
+elif service=="Good":
+     print(f"Tip is {z}")
+     tip=0.20
+    
+elif service=="Great":
+      print(f"Tip is {w}")
+      tip=0.25
+else:
+     print("invalid")
+
