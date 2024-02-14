0e32ac7dfa3ce1176f2405872dec596e4f3fd52d
HongzhengWu51 <HOngzhengw2789@nycstudents.net>
2024-02-13 21:53:16 -0500

    kikk

--- GCF2.py
+++ GCF2.py
@@ -1,18 +1,17 @@
 #return:returns the output back to the person.
-def factor_both(x,y):
+def compare(x,y):
     smaller=0
     if x > y:
         smaller=y
     else:
         smaller=x
-    gcf=0
-    for i in range(1,smaller+1):
+    
+    for i in range(1, smaller+1):
         if(x % i == 0) and (y % i ==0):
-           gcf=i
-        return gcf
+            print("gcf=(i)")
          
 number1=30
 number2=10
-print("gcf is", factor_both(number1,number2))
+print("gcf is", compare(number1,number2))
 
 
