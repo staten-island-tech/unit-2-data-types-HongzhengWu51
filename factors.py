d1a2a73fff43fa5d293c3e43127664d2a5714858
HongzhengWu51 <HOngzhengw2789@nycstudents.net>
2024-02-13 19:00:16 -0500

    cc

--- factors.py
+++ factors.py
@@ -1,8 +1,9 @@
-#First we need to make a function that allows and input.
+#First we need to make a function that allows and input. The word input in () allows person to put an input.
 #for i in range creates a loop. Here it tells the range from 1 to x+1. But it's exclusive so it excluedes one.
 #This makes it from 1 to x.
 #The % symbol is used for finding the remainder in division.
-#The code means that if the remainder is zero then print that number.
+#The code means that if x divided by i creates zero then it means that it is a factor. So we print i.
+#I also don't really understand so I don't know. 
 
 
 
