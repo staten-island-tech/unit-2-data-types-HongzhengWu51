e14392f78b2219d889552710d1b68693cd723e2b
HongzhengWu51 <HOngzhengw2789@nycstudents.net>
2024-02-14 21:37:13 -0500

    ccc

--- GCF2.py
+++ GCF2.py
@@ -1,13 +1,24 @@
 #return:returns the output back to the person.
+#How to find GCF in general. Find factors of both and use the greatest common one. gcf cant exceed that of smaller number's gcf.
+
+x=(input("pick a number:"))
+y=(input("Pick another number:"))
+
+
 def compare(x,y):
+    smaller=0
     if x > y:
         smaller=y
     else:
         smaller=x
-    for i in range(1,smaller+1):
+
+    gcf=1
+    for i in range(1, smaller+1):
         if(x % i == 0) and (y % i ==0):
             gcf=i
-        return gcf
-number1=6
-number2=8
-print("gcf is:", compare(number1,number2))
+         
+number1=30
+number2=10
+print("gcf of" ,x, "and" ,y, "is",gcf)
+
+
