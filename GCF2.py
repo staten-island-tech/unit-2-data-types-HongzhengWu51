c0ba085e8ab407f48d7c137adad86308cdf21b6a
HongzhengWu51 <HOngzhengw2789@nycstudents.net>
2024-02-14 22:04:13 -0500

    GCF

--- GCF2.py
+++ GCF2.py
@@ -1,24 +1,32 @@
-#return:returns the output back to the person.
+#return: returns the output back to the person.
 #How to find GCF in general. Find factors of both and use the greatest common one. gcf cant exceed that of smaller number's gcf.
+#Return needs to be used in order to leave the function and calls the thing so that it can be used. 
+#Return is useful when there are many functions using the same variable like x and it wouldn't work becuase it has different values.
+#EX:         gcf = i, smaller, x
+#         return gcf, a, b, c.        It would print it in that order.
+# x = int(input("pick a number: "))
+#y = int(input("Pick another number: "))
+#It wouldn't work because it was a string and can't be converted to smaller+1 because it wasn't an integer.
 
-x=(input("pick a number:"))
-y=(input("Pick another number:"))
+x = int(input("pick a number: "))
+y = int(input("Pick another number: "))
 
 
-def compare(x,y):
-    smaller=0
+def find_gcf(x, y):
+    smaller = 0
     if x > y:
-        smaller=y
+        smaller = y
     else:
-        smaller=x
+        smaller = x
 
-    gcf=1
-    for i in range(1, smaller+1):
-        if(x % i == 0) and (y % i ==0):
-            gcf=i
-         
-number1=30
-number2=10
-print("gcf of" ,x, "and" ,y, "is",gcf)
+    gcf = 1
+    for i in range(1, smaller + 1):
+        if (x % i == 0) and (y % i ==0):
+            gcf = i
 
+    return gcf
+
+
+result = find_gcf(x, y)
+print("gcf of ", x, "and " , y, "is", result)
 
