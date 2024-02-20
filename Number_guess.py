9bc66ba7b633012f1ac52d7c2566ea12047580d2
HongzhengWu51 <HOngzhengw2789@nycstudents.net>
2024-02-19 22:58:41 -0500

    commit

--- Number_guess.py
+++ Number_guess.py
@@ -1,5 +1,33 @@
-import random
+from random import randrange
 
-list=(['1','2','3','4','5','6','7','9','10'])
-print(random.choice(list))
+# number_list = ['1','2','3','4','5','6','7','9','10']
+# print(random.choice(number_list))
 
+min = 1
+max = 10
+random_number = randrange(min, max)
+
+
+user_guess = 0
+guess_history = []
+while True:
+    user_guess = int(input(f'\nGuess an integer number between {min} and {max}: '))
+
+    if user_guess == random_number:
+        
+        break
+    elif user_guess > random_number:
+        print('The guess is greater than the number')
+    else:
+        print('The guess is less than the number')
+
+    guess_history.append(user_guess)
+
+    print('Already guessed: ', end=' ')
+    for guess in guess_history:
+        print(guess, end=' ')
+
+print('Congratulations! The guess is correct!')
+print('Guess history: ', end=' ')
+for guess in guess_history:
+    print(guess, end=' ')
