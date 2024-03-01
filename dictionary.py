54bd94878dc69765fa416bd8b2fc7e93a9524174
HongzhengWu51 <158204281+HongzhengWu51@users.noreply.github.com>
2024-03-01 12:27:23 -0500

    cc

--- dictionary.py
+++ dictionary.py
@@ -1,21 +1,20 @@
-books = [ { 
-    'Title': "Crime and Punishment",
-    'Genre': "Novel",
-    'Characters':["Rodion Raskolnikov", "Porfiry Petrovitch", "Sonia Marmeladova"]
-}, {
-    'Title': "The Great Gatsby",
-    'Genre': "Novel",
-    'Characters': ["Jay Gatsby", "Mr. Gatz", "Nick Carraway"] 
-}, {
-    'Title': "Death of a Salesman",
-    'Genre': "Plays",
-    'Characters': ["Willy Loman", "Biff Loman", "Happy Loman"]
-}, {
-    'Title': "No Longer Human",
-    'Genre': "Novel",
-    'Characters': ["Yozo", "Takeichi"]
-}]
+Search= (input("Search fir a genre"))
 
+books = [ {'Title': "Crime and Punishment",
+    'genre': "psycological Novel",
+    'characters':["Rodion Raskolnikov", "Porfiry Petrovitch", "Sonia Marmeladova"],
+}, {'title': "The Great Gatsby",
+    'genre': "Tragedy",
+    'characters': ["Jay Gatsby", "Mr. Gatz", "Nick Carraway"], 
+}, {'title': "Death of a Salesman",
+    'genre': "Plays",
+    'characters': ["Willy Loman", "Biff Loman", "Happy Loman"],
+}, {'title': "No Longer Human",
+    'genre': "Novel",
+    'characters': ["Yozo", "Takeichi"],
+}]
 def get_book(books):
     for book in books():
-        if "Crime" in 
+        if 'Rodion Raskolnikov' in book ['characters']:
+            print(book['genre'], book['title'])
+get_book(books)
