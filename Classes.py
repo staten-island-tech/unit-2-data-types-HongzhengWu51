cfb5c9ac44114d46ba19c721a91acb217c34630b
HongzhengWu51 <158204281+HongzhengWu51@users.noreply.github.com>
2024-03-19 12:29:11 -0400

    cccc

---
+++ Classes.py
@@ -0,0 +1,24 @@
+import datetime
+today = datetime.date.today()
+year = today.year
+
+class calculator():
+    def add(x,y):
+        return (x+y)
+    def multiple(x,y):
+        return x * y
+print(calculator.add(7,6))
+
+class Movie():
+    def_init_(self,title,year,genre):
+    self.title= title
+    self.year= year
+    self.genre= genre 
+def_str(self):
+    returnf"{self.title},{self.year}"
+
+
+Star_Wars= Movie("Star Wars", 1977,['Sci-Fi'])
+Avengers = Movie("Avengers", 2014,['Comic'])
+print(Avengers)
+print(Avengers.how_old)
