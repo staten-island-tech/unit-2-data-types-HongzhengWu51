8aec98e0428f608a71e748251234dda3015bf75a
HongzhengWu51 <158204281+HongzhengWu51@users.noreply.github.com>
2024-03-20 12:27:39 -0400

    movie

--- Movie_database.py
+++
@@ -1,5 +0,0 @@
-import json
-## Open the JSON file of movie data
-movies = open("./movies.json", encoding="utf8")
-## create variable "data" that represents the enitre movie list
-data = json.load(movies)
