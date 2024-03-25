210d9197fb5236a331e22c54ed517a13ebda2489
HongzhengWu51 <HOngzhengw2789@nycstudents.net>
2024-03-25 00:38:12 -0400

    cdjnfb

--- gradebook.py
+++ gradebook.py
@@ -1,14 +1,15 @@
-name = (input("type your name:"))
-student_class= (input("type your class:"))
-grades = (input("type in your grades :")).split()
+name = (input("type your name: "))
+student_class= (input("type your class: "))
+grades = (input("type in your grades (separated by spaces): ")).split()
 grades = [int(grade) for grade in grades]
+
 
 student = {
     'name' : name,
     'class' : student_class,
     'grades': grades,
 }
-#Finds average. Len finds how many numbers there are.
+
 def get_average(grades):
     total = sum(grades)
     average = total / len(grades)
